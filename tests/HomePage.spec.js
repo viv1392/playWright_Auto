c2bb1e72c380d7d7ccfdb2bca60f6c6f0ced95a5
viv1392 <vivpandeyy@gmail.com>
2025-08-12 09:58:12 +0530

    Commands

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -13,6 +13,17 @@ test("Home Page Test", async ({ page }) => {
     console.log(`Current URL is: ${currentUrl}`);
     await expect(page).toHaveURL('https://demoblaze.com/');
     page.close();
+
+   // Some command to run tests
+   
+  // npx playwright test    ==> runs all the test in headles mode
+  //npx playwright test HomePage.spec.js    ==> runs only the HomePage.spec.js test file
+  //npx playwright test HomePage.spec.js --headed    ==> runs the HomePage.spec in headed mode
+ //npx playwright test HomePage.spec.js --headed --debug    ==> runs the HomePage.spec in headed mode with debug enabled 
+ //npx playwright test HomePage.spec.js --headed --debug --project=chromium    ==> runs the HomePage.spec in headed mode with debug enabled for chromium browser
+ //  npx playwright test HomePage.spec.js --headed --debug --project=chromium --grep @smoke    ==> runs the HomePage.spec in headed mode with debug enabled for chromium browser and only runs tests tagged with @smoke
+ //  npx playwright test HomePage.spec.js --headed --slow mo 1000    ==> runs the HomePage.spec in headed mode with debug enabled and slows down the test execution by 1 second   
+    
     
     
 })
