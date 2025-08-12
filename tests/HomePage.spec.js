2d92cbac7eb1b1de838c4c66ca3d7bff2da31571
viv1392 <vivpandeyy@gmail.com>
2025-08-12 09:43:23 +0530

    validation

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -2,19 +2,17 @@ const { test, expect } = require('@playwright/test');
 test("Home Page Test", async ({ page }) => {
     // Navigate to the home page
     await page.goto('https://demoblaze.com/');
+    const pageTitle = await page.title();
+    console.log(`Page title is: ${pageTitle}`);
     
     // Check if the title is correct
     await expect(page).toHaveTitle('STORE');
     
-    // Check if the main heading is present
-    const mainHeading = page.locator('a');
-    await expect(mainHeading).toHaveText('PRODUCT STORE');
+    // Check if the url is correct
+    const currentUrl = page.url();
+    console.log(`Current URL is: ${currentUrl}`);
+    await expect(page).toHaveURL('https://demoblaze.com/');
+    page.close();
     
-    // Check if the paragraph text is present
-   // const paragraph = page.locator('p');
-    //await expect(paragraph).toHaveText('This domain is for use in illustrative examples in documents.');
     
-    // Check if the link is present and has the correct URL
-   // const link = page.locator('a');
-   // await expect(link).toHaveAttribute('href', 'https://www.iana.org/domains/example');
-    })
+})
